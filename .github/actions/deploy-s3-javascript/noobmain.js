269ca112ae8365daf56bf9b9a108fb7cdfd652d9
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-11-01 06:32:03 -0500

    Added method URL

--- .github/actions/deploy-s3-javascript/noobmain.js
+++ .github/actions/deploy-s3-javascript/noobmain.js
@@ -13,7 +13,7 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    const websiteURL = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
     core.setOutput('website-url', websiteURL);
 
 }
